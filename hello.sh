d479918de1ef125b58ae31702dd0b324ee6072c5
hkhesro <hkhosro5@gmail.com>
2023-05-14 15:13:11 +0300

    error

--- hello.sh
+++ hello.sh
@@ -1,1 +1,130 @@
-echo "Hello hkhesro!"
+Script started on 2023-05-14 14:17:56+03:00 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="187" LINES="13"]
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./hello.sh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./hello.sh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./hello.sh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ cat hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ cat hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ cat hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ cd hello.sh
+[?2004lbash: cd: hello.sh: Not a directory
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ chmod =[K+c[Kx hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ cat hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./hello.sh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ pwd
+[?2004l/home/hkhesro/bh-piscine
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./hello.sj[Kh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add[K[K[K[K[K[K[K./hello.sh
+[?2004lbash: ./hello.sh: Text file busy
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git af[Kdd hello.sh[K[K[K[K[K[K[K[K[K[K[K[K[K[K[K[Kcd hello.sh
+[?2004lbash: cd: hello.sh: Not a directory
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add hello.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git commit -m "My ver [Ky first commit"
+[?2004l[master 06c2e01] My very first commit
+ 1 file changed, 1 insertion(+)
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git push
+[?2004lEnumerating objects: 5, done.
+Counting objects:  20% (1/5)Counting objects:  40% (2/5)Counting objects:  60% (3/5)Counting objects:  80% (4/5)Counting objects: 100% (5/5)Counting objects: 100% (5/5), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 267 bytes | 267.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+remote: . Processing 1 references[K
+remote: Processed 1 references in total[K
+To https://learn.reboot01.com/git/hkhesro/bh-piscine.git
+   0507e3a..06c2e01  master -> master
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [K]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [K]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [K]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [K]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ pwd
+[?2004l/home/hkhesro/bh-piscine
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [7mcurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'[27m[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ccurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'[1P\"}}){id}}"}'[1P\"}}){id}}"}'[1P\"}}){id}}"}'[1P\"}}){id}}"}'[1P\"}}){id}}"}'[1P\"}}){id}}"}'h\"}}){id}}"}'k\"}}){id}}"}'h\"}}){id}}"}'e\"}}){id}}"}'s\"}}){id}}"}'r\"}}){id}}"}'o\"}}){id}}"}'
+[?2004l{"errors":[{"extensions":{"code":"validation-failed","path":"$.selectionSet.user"},"message":"field 'user' not found in type: 'query_root'"}]}[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ curl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"hkhesro\"}}){id}}"}'[1P}}"}'[1P}}"}'2}}"}'3}}"}'1}}"}'7}}"}'4}}"}'8}}"}'
+[?2004l{"errors":[{"extensions":{"code":"validation-failed","path":"$.query"},"message":"not a valid graphql query"}]}[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ curl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"hkhesro\"}}){231748}}"}'[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Kcurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"hkhesro\"}}){231748}}"}'[4Pid}}"}'[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cpwd[Kgit pushcommit -m "My very first commit"[20Padd hello.sh[5Pc[C[C[C[C[C[C[C[C[C[C[1P./hello.shcd hello.sh[5@git ad[C[C[C[C[C[C[C[C[C[Ccommit -m "My very first commit"push[K[5Ppwdcurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"hkhesro\"}}){id}}"}'231748}}"}'[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[K
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [7mcurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'[27m
+
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ccurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'
+
+[A
+[?2004l{"errors":[{"extensions":{"code":"validation-failed","path":"$.selectionSet.user"},"message":"field 'user' not found in type: 'query_root'"}]}[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ [7mcurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'[27m
+
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ccurl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'
+
+[A
+[?2004l{"errors":[{"extensions":{"code":"validation-failed","path":"$.selectionSet.user"},"message":"field 'user' not found in type: 'query_root'"}]}[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ curl -s https://learn.reboot01.com/api/graphql-engine/v1/graphql --data '{"query":"{user(where:{login:{_eq:\"choumi\"}}){id}}"}'
+[?2004l{"errors":[{"extensions":{"code":"validation-failed","path":"$.selectionSet.user"},"message":"field 'user' not found in type: 'query_root'"}]}[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ '
+[?2004l[?2004h> 
+[?2004l[?2004h> git add
+[?2004l[?2004h> ^C[?2004l[?2004h[?2004l
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add to-git[K[K[K[K[K[K
+[?2004lNothing specified, nothing added.
+[33mhint: Maybe you wanted to say 'git add .'?[m
+[33mhint: Turn this message off by running[m
+[33mhint: "git config advice.addEmptyPathspec false"[m
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add to-git-or-not-to-git.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git push
+[?2004lEverything up-to-date
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git push
+[?2004lEverything up-to-date
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git pushadd to-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Po-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.sh.to-git-or-not-to-git.sh/to-git-or-not-to-git.sh
+[?2004lbash: ./to-git-or-not-to-git.sh: Permission denied
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ./to-git-or-not-to-git.sh[C[1Pto-git-or-not-to-git.sh[1Pto-git-or-not-to-git.shcto-git-or-not-to-git.shhto-git-or-not-to-git.shmto-git-or-not-to-git.shoto-git-or-not-to-git.shdto-git-or-not-to-git.sh to-git-or-not-to-git.sh7to-git-or-not-to-git.sh5to-git-or-not-to-git.sh5to-git-or-not-to-git.sh to-git-or-not-to-git.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ chmod 755 to-git-or-not-to-git.sh[8P./to-git-or-not-to-git.sh
+[?2004l 
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add to-git-or-not-to-git.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git commit -m "Skip"
+[?2004l[master a28e853] Skip
+ 1 file changed, 1 insertion(+)
+ create mode 100755 to-git-or-not-to-git.sh
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git push
+[?2004lEnumerating objects: 4, done.
+Counting objects:  25% (1/4)Counting objects:  50% (2/4)Counting objects:  75% (3/4)Counting objects: 100% (4/4)Counting objects: 100% (4/4), done.
+Delta compression using up to 8 threads
+Compressing objects:  50% (1/2)Compressing objects: 100% (2/2)Compressing objects: 100% (2/2), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 284 bytes | 284.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+remote: . Processing 1 references[K
+remote: Processed 1 references in total[K
+To https://learn.reboot01.com/git/hkhesro/bh-piscine.git
+   06c2e01..a28e853  master -> master
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git pushcommit -m "Skip"add to-git-or-not-to-git.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add to-git-or-not-to-git.shpush[Kcommit -m "Skip"
+[?2004l[master 1beddfc] Skip
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git commit -m "Skip"add to-git-or-not-to-git.sh[11Pcommit -m "Skip"[Kgit commit -m "Skip"add to-git-or-not-to-git.shpush[K
+[?2004lEnumerating objects: 5, done.
+Counting objects:  20% (1/5)Counting objects:  40% (2/5)Counting objects:  60% (3/5)Counting objects:  80% (4/5)Counting objects: 100% (5/5)Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects:  50% (1/2)Compressing objects: 100% (2/2)Compressing objects: 100% (2/2), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 288 bytes | 288.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+remote: . Processing 1 references[K
+remote: Processed 1 references in total[K
+To https://learn.reboot01.com/git/hkhesro/bh-piscine.git
+   a28e853..1beddfc  master -> master
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git pushcommit -m "Skip"add to-git-or-not-to-git.sh
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add to-git-or-not-to-git.shpush[Kcommit -m "Skip"
+[?2004l[master 3ee20e7] Skip
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git commit -m "Skip"add to-git-or-not-to-git.shpush[K
+[?2004lEnumerating objects: 5, done.
+Counting objects:  20% (1/5)Counting objects:  40% (2/5)Counting objects:  60% (3/5)Counting objects:  80% (4/5)Counting objects: 100% (5/5)Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects:  50% (1/2)Compressing objects: 100% (2/2)Compressing objects: 100% (2/2), done.
+Writing objects:  33% (1/3)Writing objects:  66% (2/3)Writing objects: 100% (3/3)Writing objects: 100% (3/3), 284 bytes | 284.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0), pack-reused 0
+remote: . Processing 1 references[K
+remote: Processed 1 references in total[K
+To https://learn.reboot01.com/git/hkhesro/bh-piscine.git
+   1beddfc..3ee20e7  master -> master
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git rm pushcommit -m "Skip"add to-git-or-not-to-git.sh[1P to-git-or-not-to-git.sh[1P to-git-or-not-to-git.sh[1P to-git-or-not-to-git.shr to-git-or-not-to-git.shm to-git-or-not-to-git.sh
+[?2004lrm 'to-git-or-not-to-git.sh'
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ 
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ 
+[?2004l[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-0045[00m:[01;34m~/bh-piscine[00m$ git add
+[?2004lNothing specified, nothing added.
+[33mhint: Maybe you wanted to say 'git add .'?[m
+[33mhint: Turn this message off by running[m
+[33mhint: "git config advice.addEmptyPathspec false"[m
+[?2004h]0;hkhesro@LAPT-0045: ~/bh-piscine[01;32mhkhesro@LAPT-00
