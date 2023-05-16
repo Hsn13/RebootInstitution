315e84dc5077bf8a6ce233552e88a99def1d73fa
unknown <local-admin@SHP-KFC01.bh.zain.com>
2023-05-16 21:50:40 +0300

    test7

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 1 ) | .connections.relatives' | sed 's/"//g'
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 1 ) | .connections.relatives' | sed 's/"//g'
