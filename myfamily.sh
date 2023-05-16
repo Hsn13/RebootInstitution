fc1d83a1c6cef1183973dfc88e5f347683069e26
unknown <local-admin@SHP-KFC01.bh.zain.com>
2023-05-16 22:05:52 +0300

    test7

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,4 @@
-curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == 70 ) | .connections.relatives' | sed 's/"//g'
+export HERO_ID=1
+curl -s https://learn.reboot01.com/assets/superhero/all.json | jq '.[] | select( .id == '$HERO_ID' ) | .connections.relatives' | sed 's/"//g'
+
+
