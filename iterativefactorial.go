7e8c0898fba04c6dab8bce42ae5e7b7366ba0bd4
hkhesro <hkhosro5@gmail.com>
2023-05-21 19:38:37 +0300

    Quest4-1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,10 +2,8 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 
-	if nb < 0 || nb > 12 {
+	if nb <= 0 || nb > 24 {
 		return 0
-	} else if nb == 0 {
-		return 1
 	}
 	a := nb - 1
 	for i := a; i >= 1; i-- {
