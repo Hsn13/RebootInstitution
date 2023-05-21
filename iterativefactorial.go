f62384df39a57dd537a94b5baa72e85bd58b64b0
hkhesro <hkhosro5@gmail.com>
2023-05-21 18:35:00 +0300

    Quest4-1

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,20 +1,12 @@
-package main
-
-import "fmt"
+package piscine
 
 func IterativeFactorial(nb int) int {
 	for nb < 0 {
 		return 0
 	}
-
 	result := 1
 	for i := 1; i <= nb; i++ {
 		result *= i
 	}
 	return result
-}
-
-func main() {
-	arg := 4
-	fmt.Println(IterativeFactorial(arg))
 }
