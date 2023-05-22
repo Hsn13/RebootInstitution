d7a0f425ebd4d5ffcf6d0558588406a8cd93746d
hkhesro <hkhosro5@gmail.com>
2023-05-22 17:54:41 +0300

    Q5-10

--- isalpha.go
+++ isalpha.go
@@ -1,10 +1,13 @@
 package piscine
 
 func IsAlpha(s string) bool {
+	if s == "" {
+		return true
+	}
 	for _, s := range s {
-		if s > 64 && s < 91 || s > 96 && s < 123 || s > 47 && s < 58 || s == 32 {
-			return true
+		if !((s >= 48 && s <= 57) || (s >= 65 && s <= 90) || (s >= 97 && s <= 122)) {
+			return false
 		}
 	}
-	return false
+	return true
 }
