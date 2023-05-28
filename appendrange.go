1f7a799c8e29ba9fd8b05403e2672187774f541f
hkhesro <hkhosro5@gmail.com>
2023-05-28 18:48:58 +0300

    Q7-1

--- appendrange.go
+++ appendrange.go
@@ -1,12 +1,12 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-	var res_arr []int
-	if int(max) <= int(min) {
-		return res_arr
+	var arr_res []int
+	if max <= min {
+		return arr_res
 	}
 	for i := min; i < max; i++ {
-		res_arr = append(res_arr, i)
+		arr_res = append(arr_res, i)
 	}
-	return res_arr
+	return arr_res
 }
